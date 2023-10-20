af8522fb032e0ab2853a70fc47c95e68892e948b
bravemind78 <bravemind78@gmail.com>
2023-10-20 19:53:42 +0300

    first commit

---
+++ lib/dart_task1.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: non_constant_identifier_names
+
+void main() {
+  String Name = 'Nader Makram';
+  String Company = 'Fresh Company for Home Appliances';
+  int Experience = 20;
+  bool Status = true;
+  double Salary = 23.8;
+  print(
+      'My Name is $Name and iam working for $Company with Experience more than $Experience years ,my salary is $Salary my working status is $Status ');
+  var City = 'Tenth of Ramadan City';
+  City = 'Madinty';
+  dynamic Year = 2020;
+  Year = 'twenty-Twenty';
+  print('living in $City since $Year');
+  print(
+      "The diffrenece between (Var)and(Dynamic) variables is :var is used to declare variables with implicit type inference....but dynamic is used to declare variables with dynamic typing. ");
+}
